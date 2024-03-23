8d1be8e341a1348190de102717dd774dd6d940a7
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2024-03-22 22:11:55 -0400

    minor update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { logout } from './slices/authSlice';
+//
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
